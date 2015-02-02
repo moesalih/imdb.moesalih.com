757dc3d6d979244b8d2eb1459e748b2047a6a29b
Moe Salih <moe.salih@gmail.com>
2015-02-01 20:56:48 -0500

    add link to original imdb page

--- php/routes/default.php
+++ php/routes/default.php
@@ -1,12 +1,8 @@
-<?php
-
-
-
-?>
 
 <br /><br /><br /><br /><br />
 
 <div class="small text-muted text-center">
-	Sorry. This page has not been redesigned. <br /><br />
-	<code><?php echo $route; ?></code>
+	Sorry. This page has not been implemented yet. <br /><br />
+	<code><?php echo $route; ?></code><br /><br />
+	View original page on <a href="http://www.imdb.com<?php echo $path; ?>" target="_blank">IMDb</a>.	
 </div>
