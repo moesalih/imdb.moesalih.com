5924b8e0a3ec46e3e572bcb91d433ee77eca3881
Moe Salih <moe.salih@gmail.com>
2015-02-16 17:56:57 -0500

    simpler about page and bitcoin donate link

--- php/routes/about.php
+++ php/routes/about.php
@@ -1,8 +1,7 @@
 
 <div class="container">
 	<h2>What is this?</h2>
-	<p>IMDb needs a better and more modern design. This is a minimal concept that provides access the awesome content IMDb has without having to suffer through their interface.
-	<p>This is not a fully functioning redesign. It's a simple concept that only covers the basics of browsing movie and actor information. Things like rating movies, the watchlist, the message boards, and editing content are not and will probably not be supported due to technical limitations.
+	<p>This is a minimal interface to the awesome content IMDb has. It only covers the basics of browsing movie and actor information. Things like rating movies, the watchlist, the message boards, and editing content are not and will probably not be supported due to technical limitations.
 	<p>The design was also highly influenced by the structure of the current IMDb site to minimize development efforts, and you might encounter some pages that are not implemented yet.
 	<p>If you like this redesign, please share it with your friends and spread the word. And if you have any feedback or if you just want to say hi, feel free to <a href="mailto:moe.salih@gmail.com">email me</a>. I'm also available for hire. If you or anyone you know need a mobile or web application designed and developed, get in touch :)
 	<p>View the source code and contribute on <a href="https://github.com/moesalih/imdb.moesalih.com" target="_blank">GitHub</a>.
@@ -12,11 +11,19 @@
 	<br /><br />
 	<hr class="seperator" />
 	<br />
-	
-	<div class="small">
+	<div class="small text-center">
 		Drag the following bookmarklet to your bookmarks toolbar to be able to quickly move from IMDb to the same page here.<br /><br />
 		<div class="text-center"><a class="btn btn-default" href="javascript:void((function(){if(document.location.hostname.indexOf('www.imdb.com')!==-1){document.location.href=document.URL.replace('www.imdb.com','imdb.moesalih.com');}else{document.location.href='http://imdb.moesalih.com/'}})());">IMDb</a></div>
 	</div>
+
+	<br />
+	<hr class="seperator" />
+	<br />
+	<div class="small text-center">
+		If you've enjoyed this, you can support it by donating any amount of bitcoins to the following address:<br /><br />
+		<code>1LHmJZD887PghYuJBRKta8EURs8oKziZ5h</code>
+	</div>
+
 </div>
 
 
