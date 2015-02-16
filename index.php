31a861a62fbc7f89a4d38363c41c280867e56b1e
Moe Salih <moe.salih@gmail.com>
2015-02-16 17:56:38 -0500

    simple yellow logo

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">imdb</a>
+      <a class="navbar-brand" href="/">IMDB</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
